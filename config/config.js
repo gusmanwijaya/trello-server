463d057936fefef12bf729ed6af6dfab20e462b3
Gusman Wijaya <gusman.w.jaya@gmail.com>
2022-03-02 20:38:52 +0700

    prepare deploy to heroku

--- config/config.js
+++ config/config.js
@@ -1,27 +1,30 @@
-require("dotenv").config();
-
-const { DB_USERNAME, DB_DATABASE, DB_HOST, DB_DIALECT } = process.env;
-
 module.exports = {
   development: {
-    username: DB_USERNAME,
+    username: "root",
     password: null,
-    database: DB_DATABASE,
-    host: DB_HOST,
-    dialect: DB_DIALECT,
+    database: "clone_trello",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
   test: {
-    username: DB_USERNAME,
+    username: "root",
     password: null,
-    database: DB_DATABASE,
-    host: DB_HOST,
-    dialect: DB_DIALECT,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
   production: {
-    username: DB_USERNAME,
-    password: null,
-    database: DB_DATABASE,
-    host: DB_HOST,
-    dialect: DB_DIALECT,
+    username: "yycrmvpuuxepjr",
+    password:
+      "7ec4aa0914405dec37977b07cd50ecc0fe1c32be4a6aa0a9fa2910705918cf02",
+    database: "d96ua4mqh2nlf5",
+    host: "ec2-3-225-79-57.compute-1.amazonaws.com",
+    port: 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
